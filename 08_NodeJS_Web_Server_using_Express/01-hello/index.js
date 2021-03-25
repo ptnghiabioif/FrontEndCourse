24a25160401c10aecdc1070f7a4ab10b669c38b2
ptnghiabioif <trungnghiamap>
2021-03-26 01:00:36 +0700

    Split User Router

--- 08_NodeJS_Web_Server_using_Express/01-hello/index.js
+++ 08_NodeJS_Web_Server_using_Express/01-hello/index.js
@@ -1,29 +1,16 @@
 var express = require('express');
-var app = express();
 var bodyParser = require('body-parser');
-var low = require('lowdb');
-var FileSync = require('lowdb/adapters/FileSync');
-var adapter = new FileSync('db.json')
 
-db = low(adapter)
-
-db.defaults({ users: [] })
-    .write();
+var userRoutes = require('./routes/user.route')
 
 var port = 3000
 
-// var users = [
-//     { id: 1, name: 'Nghia' },
-//     { id: 2, name: 'Trang' },
-//     { id: 3, name: 'Anh' }
-// ];
+var app = express();
+app.set('view engine', 'pug');
+app.set('views', './views');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-app.set('view engine', 'pug');
-app.set('views', './views');
 
 app.get('/', function (req, res) {
     res.render('index', {
@@ -31,30 +18,7 @@ app.get('/', function (req, res) {
     });
 });
 
-app.get('/users', function (req, res) {
-    res.render('users/index', {
-        users: db.get('users').value()
-    });
-});
-
-app.get('/users/search', function (req, res) {
-    var q = req.query.q;
-    var matchedUsers = db.get('users').value().filter(function (user) {
-        return user.name.toLowerCase().indexOf(q.toLowerCase()) !== -1;
-    })
-    res.render('users/index', {
-        users: matchedUsers
-    })
-});
-
-app.get('/users/create', function (req, res) {
-    res.render('users/create');
-});
-
-app.post('/users/create', function (req, res) {
-    db.get("users").push(req.body).write();
-    res.redirect('/users');
-});
+app.use('/users', userRoutes)
 
 app.listen(port, function () {
     console.log('Server listening on port ' + port);
