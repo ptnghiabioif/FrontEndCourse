900e44fc5cfbb770941f475dd614804203d13f49
ptnghiabioif <trungnghiamap>
2021-03-31 11:06:11 +0700

    Use singed cookie and env variables

--- 08_NodeJS_Web_Server_using_Express/01-hello/middlewares/auth.middleware.js
+++ 08_NodeJS_Web_Server_using_Express/01-hello/middlewares/auth.middleware.js
@@ -1,17 +1,25 @@
 var db = require('../db')
 
 module.exports.requireAuth = function(req, res, next) {
-    if (!req.cookies.userId) {
+    // console.log(req.cookies, req.signedCookies)
+    if (!req.signedCookies.userId) {
         res.redirect('/auth/login');
         return;
     }
 
-    var user = db.get('users').find({ id: req.cookies.userId}).values();
+    var user = db.get('users').find({ 
+        id: req.signedCookies.userId
+
+    }).values();
 
     if (!user) {
         res.redirect('/auth/login')
         return;
-    }
+    };
+    // console.log(user)
+    res.locals.user = user;
+
+    // console.log(res.locals.user)
     next();
 
 };
